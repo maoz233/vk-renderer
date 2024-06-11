1f991b25f64012ab9e923a934e330c505f2e80be
MaoZ <mao.zhang233@gmail.com>
2024-06-10 20:42:34 -0500

    make function proxies static methods

--- include/renderer.h
+++ include/renderer.h
@@ -43,15 +43,15 @@ class Renderer {
 
   std::vector<const char*> getRequiredExtensions();
   bool checkValidationLayerSupport();
-  VkResult CreateDebugUtilsMessengerEXT(
+
+  static VkResult CreateDebugUtilsMessengerEXT(
       VkInstance instance,
       const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
       const VkAllocationCallbacks* pAllocator,
       VkDebugUtilsMessengerEXT* pDebugMessenger);
-  void DestroyDebugUtilsMessengerEXT(VkInstance instance,
-                                     VkDebugUtilsMessengerEXT debugMessenger,
-                                     const VkAllocationCallbacks* pAllocator);
-
+  static void DestroyDebugUtilsMessengerEXT(
+      VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger,
+      const VkAllocationCallbacks* pAllocator);
   static VKAPI_ATTR VkBool32 VKAPI_CALL
   debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageServity,
                 VkDebugUtilsMessageTypeFlagsEXT messageType,
