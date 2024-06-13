de20b4970a1f6473dafe9f97d1b7425ea03ce5c4
MaoZ <mao.zhang233@gmail.com>
2024-06-12 20:22:20 -0500

    fix issue of reading shader file

--- src/renderer.cc
+++ src/renderer.cc
@@ -658,6 +658,8 @@ std::vector<char> Renderer::readFile(const std::string& filename) {
 
   size_t fileSize = static_cast<size_t>(file.tellg());
   std::vector<char> buffer(fileSize);
+  file.seekg(0);
+  file.read(buffer.data(), fileSize);
 
   file.close();
 
